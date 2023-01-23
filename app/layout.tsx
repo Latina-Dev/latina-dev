c3020c22dc1f6c10aebdba27ac1cab73e79966b6
Frances Coronel <hello@francescoronel.com>
2023-01-23 13:01:19 -0800

    fix styling
    Signed-off-by: Frances Coronel <hello@francescoronel.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "/styles/globals.css";
+import "/styles/_styles.css";
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 import GitHubCorner from "@/components/GitHubCorner/GitHubCorner";
-import { Inter } from "@next/font/google";
-import Image from "next/image";
+import Logo from "@/components/Logo/Logo";
 import Link from "next/link";
 import { description, title } from "./head";
 import styles from "./page.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -13,13 +10,8 @@ export default function Home() {
       <GitHubCorner />
       <div className={styles.container}>
         <div className={styles.center}>
-          <div className={styles.logo}>
-            <Image
-              src="/img/logos/logo-white.svg"
-              alt="Latina Dev Logo"
-              width={35}
-              height={66}
-            />
+          <div className={styles.logoContainer}>
+            <Logo className={styles.logo} />
           </div>
           <Link
             href="https://github.com/FrancesCoronel/latina-dev"
@@ -27,12 +19,12 @@ export default function Home() {
             target={"_blank"}
             className={styles.nameLink}
           >
-            <h1 className={inter.className}>{title}</h1>
+            <h1>{title}</h1>
           </Link>
           <div className={styles.description}>
-            <p className={inter.className}>{description}</p>
+            <p>{description}</p>
             <br />
-            <p className={inter.className}>More to come soon...</p>
+            <p>More to come soon...</p>
           </div>
         </div>
       </div>
