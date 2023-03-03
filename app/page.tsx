bf268da537bbd917b7789e78d837ea75236122e2
EllaCodes4 <105608215+EllaCodes4@users.noreply.github.com>
2023-03-03 16:47:30 -0600

    Added maintainer section

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import {
   faGithub,
   faLinkedin,
   faDiscord,
+  faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -40,6 +41,7 @@ export default function Home() {
             </Link>
           </section>
           <hr className={styles.heroborder} />
+          
           <section className={styles.about} id="#about">
             <h2>About Us</h2>
             <strong>
@@ -60,6 +62,61 @@ export default function Home() {
             >
               Join us on Discord <FontAwesomeIcon icon={faDiscord} />
             </Link>
+          </section>
+
+          <section className={styles.maintainers}>
+            <h3>Chief Maintainers</h3>
+            <div className="mt-10 grid grid-rows-1 grid-cols-2">
+              <div>
+                <img src="/img/maintainers/frances.jpg"></img>{" "}
+                <h4 className="mt-3">Frances Coronel</h4>
+                <small>Senior Software Enginner at JupiterSpark</small>
+                <div className={styles.maintainersociallinks}>
+                  <Link
+                    href="https://www.linkedin.com/in/francescoronel/"
+                    aria-label="Frances Coronel LinkedIn"
+                    target={"_blank"}
+                  >
+                    <FontAwesomeIcon icon={faLinkedin} />
+                  </Link>
+                  <Link
+                    href="https://github.com/FrancesCoronel"
+                    aria-label="Frances Coronel GitHub"
+                    target={"_blank"}
+                  >
+                    <FontAwesomeIcon icon={faGithub} />
+                  </Link>
+                </div>
+              </div>
+              <div>
+                <img src="/img/maintainers/gabriellac.jpg"></img>
+                <h4 className="mt-3">Gabriella Corales</h4>
+                <small>Educator turned Aspiring Front End Developer</small>
+                <div className={styles.maintainersociallinks}>
+                  <Link
+                    href="https://www.linkedin.com/in/gabriella-corales/"
+                    aria-label="Gabriella Corales LinkedIn"
+                    target={"_blank"}
+                  >
+                    <FontAwesomeIcon icon={faLinkedin} />
+                  </Link>
+                  <Link
+                    href="https://github.com/EllaCodes4"
+                    aria-label="Gabriella Corales GitHub"
+                    target={"_blank"}
+                  >
+                    <FontAwesomeIcon icon={faGithub} />
+                  </Link>
+                  <Link
+                    href="https://twitter.com/EllaCodes"
+                    aria-label="Gabriella Corales Twitter"
+                    target={"_blank"}
+                  >
+                    <FontAwesomeIcon icon={faTwitter} />
+                  </Link>
+                </div>
+              </div>
+            </div>
           </section>
 
           <footer className={styles.footer}>
