06cc1ccd586443497c2932473e07d0a62918cb4b
EllaCodes4 <105608215+EllaCodes4@users.noreply.github.com>
2023-03-29 20:08:33 -0500

    Added test json and api folder. Updated SWR. Added Members page with basic HTML & CSS. Added Members component--info not yet displaying, forthcoming

---
+++ app/members/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import GitHubCorner from "@/components/GitHubCorner/GitHubCorner";
+import styles from "./members.module.css";
+import Members from "@/components/Members/Members";
+
+export default function MembersPage() {
+  return (
+    <section className={styles.members}>
+      <GitHubCorner />
+      <div className={styles.container}>
+        <div className={styles.center}>
+          <h1 className={styles.title}>Members</h1>
+          <Members />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ components/Members/Members.tsx
@@ -0,0 +1,26 @@
+//useSWR allows the use of SWR inside function components
+import useSWR from "swr";
+
+//Write a fetcher function to wrap the native fetch function and return the result of a call to url in json format
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function Members() {
+  //Set up SWR to run the fetcher function when calling "/api/staticdata"
+  //There are 3 possible states: (1) loading when data is null (2) ready when the data is returned (3) error when there was an error fetching the data
+  const { data, error } = useSWR("/api/staticdata", fetcher);
+
+  //Handle the error state
+  if (error) return <div>Failed to load</div>;
+  //Handle the loading state
+  if (!data) return <div>Loading...</div>;
+  //Handle the ready state and display the result contained in the data object mapped to the structure of the json file
+  return (
+    <div>
+      <h3>Member Test- My Framework from file</h3>
+      <ul>
+        <li>Name: {data.record.name}</li>
+        <li>Language: {data.record.language}</li>
+      </ul>
+    </div>
+  );
+}
