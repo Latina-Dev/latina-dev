bf56d06324c85d3fd36fefde83cad2edfe702bf9
Frances Coronel <hello@francescoronel.com>
2023-04-01 12:42:26 -0700

    Update Members.tsx
    attempt to fix type error

--- components/Members/Members.tsx
+++ components/Members/Members.tsx
@@ -2,7 +2,7 @@
 import useSWR from "swr";
 
 //Write a fetcher function to wrap the native fetch function and return the result of a call to url in json format
-const fetcher = (url) => fetch(url).then((res) => res.json());
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function Members() {
   //Set up SWR to run the fetcher function when calling "/api/staticdata"
