0b825771715610d640256e1583a37283ecc4b2db
Frances Coronel <hello@francescoronel.com>
2023-05-23 15:32:40 -0700

    Updating new prop
    Affiliation prop needed to be marked optional in interface (even though we don't yet have validation) and then also I updated comment to reflect what it means

--- types/members.ts
+++ types/members.ts
@@ -2,7 +2,6 @@ type MemberLevel = "Student" | "Individual Contributor" | "Leader";
 
 export interface MemberInterface {
   name: string; // full name, e.g. Frances Coronel
-  affiliation: string // company or school, eg. Google
   added: string; // when member was added, e.g. 2023-05-07
   level: MemberLevel;
   linkedin: string; // LinkedIn vanity handle, e.g. frances-coronel from "in/frances-coronel"
@@ -11,5 +10,6 @@ export interface MemberInterface {
   github?: string; // optional: GitHub username, e.g. FrancesCoronel
   twitter?: string; // optional: Twitter username, e.g. FrancesCoronel
   website?: string; // optional: Personal website URL, e.g. https://francescoronel.com
-  bio?: string; // optional: Markdown content
+  bio?: string; // optional: Markdown that forms bio, e.g. Frances Coronel is a senior software engineer...
+  affiliation?: string // optional: title and company or school, eg. Senior Software Engineer at XYZ
 }
