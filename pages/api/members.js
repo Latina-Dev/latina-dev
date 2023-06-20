45a107530f728789ae0783a7d4085ad11f06d3a9
Amandha W. Barok <amandha.barok@gmail.com>
2023-06-20 16:52:53 -0400

    create the directory and the endpoint

---
+++ pages/api/members.js
@@ -0,0 +1,18 @@
+import clientPromise from "../../database/mongodb";
+
+export default async (req, res) => {
+    try {
+        const client = await clientPromise;
+
+        const db = client.db("latinasdev");
+
+        const members = await db
+            .collection("members")
+            .find({})
+            .toArray();
+
+        res.json(members)
+    } catch (e) {
+        console.log(e);
+    }
+}
