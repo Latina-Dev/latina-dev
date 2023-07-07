ad057e504ea2412049d6fea022d312b8125a142c
Amandha W. Barok <amandha.barok@gmail.com>
2023-07-07 09:05:10 -0400

    Small change'

--- pages/api/members.js
+++ pages/api/members.js
@@ -11,7 +11,7 @@ export default async (req, res) => {
             .find({})
             .toArray();
 
-        res.json(members)
+        res.json(members);
     } catch (e) {
         console.log(e);
     }
