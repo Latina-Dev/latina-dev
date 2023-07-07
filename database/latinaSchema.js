20dff8a81013a3f061eb563c4287de4792beb428
Amandha W. Barok <amandha.barok@gmail.com>
2023-07-07 09:35:52 -0400

    Since we decided for this moment not use mongoose, I comment this code, but probably we will use it in the future

--- database/latinaSchema.js
+++ database/latinaSchema.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const latinaSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
-        required: true
-    },
-    linkedin: {
-        type: String,
-        required: false
-    },
-    github: {
-        type: String,
-        required: false
-    }
-});
+// const latinaSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     image: {
+//         type: String,
+//         required: true
+//     },
+//     linkedin: {
+//         type: String,
+//         required: false
+//     },
+//     github: {
+//         type: String,
+//         required: false
+//     }
+// });
 
-module.exports = mongoose.model('latina', latinaSchema);
+// module.exports = mongoose.model('latina', latinaSchema);
